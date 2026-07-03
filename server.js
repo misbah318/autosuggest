56f6cde617b8128c022ffc381a8ed91ff2b4d975
misbah318 <misbah.reach@gmail.com>
2026-07-03 12:06:26 +0530

    web setup

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app=express()
+const PORT=3002;
+app.listen(PORT,function(){
+    app.use(express.static("frontend"))
+    console.log('Successfully running at http://localhost:'+PORT)
+})
