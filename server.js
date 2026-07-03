1410687d5be811fa6223c2dcb0dec0f4e59cd0ed
misbah318 <misbah.reach@gmail.com>
2026-07-03 14:17:56 +0530

    js updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app=express()
-const PORT=process.env.PORT;
+const PORT=process.env.PORT|| 3002
 app.listen(PORT,function(){
     app.use(express.static("frontend"))
     console.log('Successfully running at http://localhost:'+PORT)
